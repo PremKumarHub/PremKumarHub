ecf8b8550e5d95375796f134781124777a30a8f5
PremKumarHub <ey78627@gmail.com>
2024-10-12 16:33:30 +0530

    uploading files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // If you have any global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,7 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-reportWebVitals();
-
