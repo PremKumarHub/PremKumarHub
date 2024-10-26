7a4110ff5de585e6aed26298b99d1681d9fcac7f
PremKumarHub <ey78627@gmail.com>
2024-10-26 10:36:40 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from "./components/common/header/Header"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
 
+
 function App() {
   return (
     <>
     <Router>
-      <Header />
-   
-    </Router>
+        
+        <Header />
+        <Switch>
+          <Route  path='/about' >
+          </Route>
+          </Switch>
+       
+      </Router>
   </>
   );
 }
